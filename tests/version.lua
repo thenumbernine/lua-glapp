e2fb0083f103d01994b99bd926a2f4d6ce496711
Christopher Moore <christopher.e.moore@gmail.com>
2017-08-26 13:23:15 +0300

    added another test

---
+++ tests/version.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env luajit
+require 'ext'
+local ffi = require 'ffi'
+local gl = require 'ffi.OpenGL'
+class(require 'glapp', {
+	initGL = function(self)
+		for _,field in ipairs{
+			'GL_VENDOR',
+			'GL_RENDERER',
+			'GL_VERSION',
+		} do
+			print(field, ffi.string(gl.glGetString(gl[field])))
+		end
+		print('GL_EXTENSIONS', '\n\t'..(ffi.string(gl.glGetString(gl.GL_EXTENSIONS)):gsub(' ','\n\t')) )
+		self.done = true
+	end,
+})():run()
