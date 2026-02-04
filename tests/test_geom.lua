3a2fe34d5fff3624d7021e2365e6f6e8b2170a37
Christopher Moore <christopher.e.moore@gmail.com>
2026-02-04 12:17:15 +0700

    more messing around

--- tests/test_geom.lua
+++ tests/test_geom.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/env luajit
 local cmdline = require 'ext.cmdline'(...)
+local op = require 'ext.op'
 local sdl = require 'sdl'
 local gl = require 'gl.setup'(cmdline.gl)
 local GLSceneObject = require 'gl.sceneobject'
@@ -16,16 +17,18 @@ function App:initGL()
 layout(location=0) in vec3 vertex;
 layout(location=0) out vec3 vertexv;
 
-layout(location=0) uniform mat4 mvProjMat;
+layout(location=0) uniform mat4 viewMat;
+layout(location=1) uniform mat4 projMat;
 
 void main() {
 	vertexv = vertex;
-	gl_Position = mvProjMat * vec4(vertex, 1.);
+	// if I'm using a gemoetry shader then I can get by without emitting a gl_Position?
+	//gl_Position = projMat * (viewMat * vec4(vertex, 1.));
 }
 ]],
 			geometryCode = [[
 // I don't suppose tesselation can be specified by uniform ... probably not when tesselation shaders exist
-#define MAX_TESS 4
+#define MAX_TESS 9
 
 layout(triangles) in;
 layout(triangle_strip, 
@@ -33,28 +36,30 @@ layout(triangle_strip,
 ) out;
 
 layout(location=0) in vec3 vertexv[];
-layout(location=0) out vec3 colorg;
+layout(location=0) out vec3 vertexg;
 
-layout(location=0) uniform mat4 mvProjMat;
+layout(location=0) uniform mat4 viewMat;
+layout(location=1) uniform mat4 projMat;
 
-void emitVtx(vec3 c, vec3 v) {
-	colorg = c;//v;
-	gl_Position = mvProjMat * vec4(v, 1.);
+void emitVtx(vec3 v) {
+	// tetrahedron, tesselated, normalized, looks ugly -- too much vertexes bunched at the corners
+	// octahedron, tesselated, normalized, looks better
+	v = normalize(v);
+
+	vertexg = v;
+	gl_Position = projMat * (viewMat * vec4(v, 1.));
 	EmitVertex();
 }
 
 // triBasis[0] = v1-v0, triBasis[1] = v2-v0, triBasis[2] = v0
-vec3 getVertex(ivec2 ij, mat3 triBasis) {
-	float fi = float(ij.x) / float(MAX_TESS);
-	float fj = float(ij.y) / float(MAX_TESS);
-	vec3 v = triBasis[0] * fi + triBasis[1] * fj + triBasis[2];
-	return v;
+vec3 getVertex(vec2 ij, mat3 triBasis) {
+	return triBasis[0] * ij.x + triBasis[1] * ij.y + triBasis[2];
 }
 
-void emitTri(ivec2 ij0, ivec2 ij1, ivec2 ij2, mat3 triBasis) {
-	emitVtx(vec3(ij0, float(MAX_TESS))/float(MAX_TESS), getVertex(ij0, triBasis));
-	emitVtx(vec3(ij1, float(MAX_TESS))/float(MAX_TESS), getVertex(ij1, triBasis));
-	emitVtx(vec3(ij2, float(MAX_TESS))/float(MAX_TESS), getVertex(ij2, triBasis));
+void emitTri(vec2 ij0, vec2 ij1, vec2 ij2, mat3 triBasis) {
+	emitVtx(getVertex(ij0, triBasis));
+	emitVtx(getVertex(ij1, triBasis));
+	emitVtx(getVertex(ij2, triBasis));
 	EndPrimitive();
 }
 
@@ -63,11 +68,18 @@ void main() {
 	// subdivide accordingly
 	// or use pixel length somehow
 
+#if 0// dot based on angle between each other
 	float dot01 = dot(vertexv[0], vertexv[1]);
 	float dot02 = dot(vertexv[0], vertexv[2]);
 	float dot12 = dot(vertexv[1], vertexv[2]);
 	float mindot = min(dot01, min(dot02, dot12));
-	
+#else	//dot based on angle from view
+	float dot0 = 1. - normalize(viewMat * vec4(vertexv[0], 1.)).z;
+	float dot1 = 1. - normalize(viewMat * vec4(vertexv[1], 1.)).z;
+	float dot2 = 1. - normalize(viewMat * vec4(vertexv[2], 1.)).z;
+	float mindot = min(dot0, min(dot1, dot2));
+#endif
+
 	// TODO convert to arclength or something and determine subdivision amount
 	const float dotAngleThreshold = 1.;
 	if (mindot < dotAngleThreshold) {
@@ -75,19 +87,32 @@ void main() {
 		triBasis[0] = vertexv[1] - vertexv[0];
 		triBasis[1] = vertexv[2] - vertexv[0];
 		triBasis[2] = vertexv[0];
-		for (int i = 0; i < MAX_TESS; ++i) {
-			for (int j = 0; j < MAX_TESS-i; ++j) {
-				emitTri(ivec2(i,j), ivec2(i+1, j), ivec2(i,j+1), triBasis);
-				if (i+1 < MAX_TESS && j < MAX_TESS-(i+1)) {
-					emitTri(ivec2(i,j+1), ivec2(i+1, j), ivec2(i+1,j+1), triBasis);
+		int tess = int((float(MAX_TESS) - 1.) * (1. - mindot) * .5 + 1.);
+		float tessf = float(tess);
+		float dt = 1./tessf;
+		for (int i = 0; i < tess; ++i) {
+			float fi = float(i) / tessf;
+			for (int j = 0; j < tess-i; ++j) {
+				float fj = float(j) / tessf;
+				emitTri(
+					vec2(fi, fj),
+					vec2(fi+dt, fj),
+					vec2(fi, fj+dt),
+					triBasis);
+				if (i+1 < tess && j < tess-(i+1)) {
+					emitTri(
+						vec2(fi, fj+dt),
+						vec2(fi+dt, fj),
+						vec2(fi+dt, fj+dt),
+						triBasis);
 				}
 			}
 		}
 	} else {
 		// emit as is
 		for (int i = 0; i < 3; ++i) {
-			gl_Position = gl_in[i].gl_Position;
-			colorg = vertexv[i];
+			vertexg = vertexv[i];
+			gl_Position = projMat * (viewMat * vec4(vertexv[i], 1.));
 			EmitVertex();
 		}
 		EndPrimitive();
@@ -95,18 +120,21 @@ void main() {
 }
 ]],
 			fragmentCode = [[
-layout(location=0) in vec3 colorg;
+layout(location=0) in vec3 vertexg;
 layout(location=0) out vec4 fragColor;
 void main() {
 	fragColor = vec4(
-		//normalize(colorg) * .4 + .6,
-		colorg,
+		normalize(vertexg) * .4 + .6,
 		1.);
 }
 ]],
 		},
+		uniforms = {
+			viewMat = self.view.mvMat.ptr,
+			projMat = self.view.projMat.ptr,
+		},
+--[[ tetrahedron
 		vertexes = {
-			dim = 3,
 			data = {
 				0, 0, 1,
 				0, math.sqrt(8/9), -1 / 3,
@@ -125,9 +153,34 @@ void main() {
 				},
 			},
 		},
-		uniforms = {
-			mvProjMat = self.view.mvProjMat.ptr,
+--]]
+-- [[ octahedron
+		vertexes = {
+			data = {
+				0, 0, 1,
+				1, 0, 0,
+				0, 1, 0,
+				-1, 0, 0,
+				0, -1, 0,
+				0, 0, -1,
+			},
 		},
+		geometry = {
+			mode = gl.GL_TRIANGLES,
+			indexes = {
+				data = {
+					0,1,2,
+					0,2,3,
+					0,3,4,
+					0,4,1,
+					5,2,1,
+					5,3,2,
+					5,4,3,
+					5,1,4,			
+				},
+			},
+		},
+--]]
 	}
 
 	gl.glEnable(gl.GL_DEPTH_TEST)
@@ -135,11 +188,19 @@ void main() {
 	gl.glLineWidth(2)
 end
 
+local hasPolygonMode
 local useLines
 function App:update()
 	App.super.update(self)
 	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
-	gl.glPolygonMode(gl.GL_FRONT_AND_BACK, useLines and gl.GL_LINE or gl.GL_FILL)
+
+	if hasPolygonMode == nil then
+		hasPolygonMode = not not op.safeindex(gl, 'glPolygonMode')
+	end
+	if hasPolygonMode then
+		gl.glPolygonMode(gl.GL_FRONT_AND_BACK, useLines and gl.GL_LINE or gl.GL_FILL)
+	end
+
 	self.globj:draw()
 end
 
