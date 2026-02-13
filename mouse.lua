66cf62db6ac475b3788c77677f50a64910996a89
Christopher Moore <christopher.e.moore@gmail.com>
2026-02-13 17:43:23 +0700

    moved orbit touch control into mouse, switched mouse to be event driven though in retrospect that is probably slower since I'm just accumulating state myself, so it's still state-driven

--- mouse.lua
+++ mouse.lua
@@ -4,83 +4,138 @@ this might end up in openglapp ...
 --]]
 
 local ffi = require 'ffi'
-local gl = require 'gl'
+local table = require 'ext.table'
+local class = require 'ext.class'
+local vec2f = require 'vec-ffi.vec2f'
 local sdl = require 'sdl'
 local SDLApp = require 'sdl.app'
-local bit = require 'bit'
-local class = require 'ext.class'
-local vec2i = require 'vec-ffi.vec2i'
-local vec2f = require 'vec-ffi.vec2f'
-local vec2d = require 'vec-ffi.vec2d'
+
+
+local fingerUpEventType
+local fingerDownEventType
+local fingerMotionEventType
+local mouseButtonUpType
+local mouseButtonDownType
+local mouseMotionEventType
+local mouseWheelEventType
+if SDLApp.sdlMajorVersion == 2 then
+	mouseButtonUpType = sdl.SDL_MOUSEBUTTONUP
+	mouseButtonDownType = sdl.SDL_MOUSEBUTTONDOWN
+	mouseMotionEventType = sdl.SDL_MOUSEMOTION
+	mouseWheelEventType = sdl.SDL_MOUSEWHEEL
+elseif SDLApp.sdlMajorVersion == 3 then
+	mouseButtonUpType = sdl.SDL_EVENT_MOUSE_BUTTON_UP
+	mouseButtonDownType = sdl.SDL_EVENT_MOUSE_BUTTON_DOWN
+	mouseMotionEventType = sdl.SDL_EVENT_MOUSE_MOTION
+	mouseWheelEventType = sdl.SDL_EVENT_MOUSE_WHEEL
+	fingerUpEventType = sdl.SDL_EVENT_FINGER_UP
+	fingerDownEventType = sdl.SDL_EVENT_FINGER_DOWN
+	fingerMotionEventType = sdl.SDL_EVENT_FINGER_MOTION
+end
 
 local Mouse = class()
 
-function Mouse:init()
-	-- this is the pixel position
-	-- it was integer in sdl2
-	-- but it is float in sdl3
-	-- sooo ...
-	-- for compat reasons for now i'll store a separate 'pixelPosf' for now and convert
-	-- but eventually it'll be self.pixelPos = vec2f()
-	self.ipos = vec2i()
-	self.pixelPosf = vec2f()	-- sdl subpixel access
+--[[
+args:
+	app = obj has .width and .height for tracking screensize
+--]]
+function Mouse:init(args)
+	self.app = args.app
 
-	-- this is the window fractional position, as a percent of the window resolution
-	self.pos = vec2d()
-	self.lastPos = vec2d()
-	self.deltaPos = vec2d()
-	self.dz = 0
+	-- state being gathered from events:
 
+	self.newWheelDelta = vec2f()
+	self.newPixelPos = vec2f()	-- pixel position, unflipped. sdl subpixel access
+	self.newLeftDown = false
+	self.newRightDown = false
+
+	-- "current" state as of mouse update
+
+	self.pos = vec2f()		-- this is the window fractional position, as a percent of the window resolution
+	self.wheelDelta = vec2f()
 	self.leftDown = false
 	self.rightDown = false
+
+	-- "last" state as of mouse update
+
+	self.lastPos = vec2f()
+	self.deltaPos = vec2f()
+
+	self.fingerPinchDelta = 0
+	self.activeFingers = {}
+	self.activeFingersInOrder = table()
 end
 
-local viewportInt = ffi.new('GLint[4]')
-
--- TODO :event() instead of :update()
+-- process this-frame vs last-frame state-changes
+-- do this last, so that your update loop is ...
+--  SDL events -> get new mouse state
+--  App:update -> handle last vew new mouse staet
+--  Mouse:update -> save new-to-last mouse states
 function Mouse:update()
-	-- store last state
-	self.lastPos.x = self.pos.x
-	self.lastPos.y = self.pos.y
+	local app = self.app
 
-	-- update new state
+	-- process multi-finger events:
+	self.doingMultiTouchGesture = nil
+	self.fingerPinchDelta = 0
+	if self.gotFingerEvent then
+		local numTouches = 0
+		local prevNumTouches = #self.activeFingersInOrder
+		-- is there a promise that activeFingers IDs are sequential?
+		for fingerID, finger in pairs(self.activeFingers) do
+			numTouches=numTouches+1
+			self.activeFingersInOrder[numTouches] = finger
+		end
+		for j=numTouches+1,prevNumTouches do
+			self.activeFingersInOrder[j] = nil
+		end
+--DEBUG:print('we currently have', numTouches, 'touches, previously', prevNumTouches)
 
-	local sdlButtons
-	if SDLApp.sdlMajorVersion == 2 then
-		sdlButtons = sdl.SDL_GetMouseState(self.ipos.s, self.ipos.s+1)
-	elseif SDLApp.sdlMajorVersion == 3 then
-		sdlButtons = sdl.SDL_GetMouseState(self.pixelPosf.s, self.pixelPosf.s+1)
-		-- and for compat for now, convert/store the integer pixel positions
-		self.ipos.x, self.ipos.y = self.pixelPosf.x, self.pixelPosf.y
-	else
-		error("SDLApp.sdlMajorVersion is unknown: "..require'ext.tolua'(SDLApp.sdlMajorVersion))
+		local f1, f2, pos1, pos2
+		if numTouches >= 2 then
+			f1, f2 = table.unpack(self.activeFingersInOrder)
+			pos1, pos2 = f1.pos, f2.pos
+			self.multiTouchDistSq = (pos2.x - pos1.x)^2 + (pos2.y - pos1.y)^2
+
+			-- tell our mouse event handler to ignore events coming from touches
+			self.doingMultiTouchGesture = true
+		else
+			self.multiTouchDistSq = nil
+		end
+
+		if self.multiTouchDistSq
+		and self.lastMultiTouchDistSq
+		and self.multiTouchDistSq ~= self.lastMultiTouchDistSq
+		then
+			local toDist = math.sqrt(self.multiTouchDistSq)
+			local fromDist = math.sqrt(self.lastMultiTouchDistSq)
+			local zoomChange = toDist - fromDist
+			self.fingerPinchDelta = zoomChange
+		end
+		self.lastMultiTouchDistSq = self.multiTouchDistSq
+		self.gotFingerEvent = nil
 	end
 
-	-- TODO use glapp for the size, in case the viewport is set to a subset of the window
-	gl.glGetIntegerv(gl.GL_VIEWPORT, viewportInt)
-	local viewWidth, viewHeight = viewportInt[2], viewportInt[3]
 
-	-- not working ... might need sdl event handling for this (i.e. openglapp)
-	self.dz = 0
-	-- TODO modern SDL uses SDL_MOUSEWHEEL event ...
-	--if bit.band(sdlButtons, bit.lshift(1, sdl.SDL_BUTTON_WHEELUP-1)) ~= 0 then self.dz = self.dz + 1 end
-	--if bit.band(sdlButtons, bit.lshift(1, sdl.SDL_BUTTON_WHEELDOWN-1)) ~= 0 then self.dz = self.dz - 1 end
-	-- sdl + mouse wheel is not working:
-	if self.dz ~= 0 then print('mousedz',self.dz) end
+	-- mouse
 
-	self.pos.x = self.ipos.x / viewWidth
-	self.pos.y = 1 - self.ipos.y / viewHeight
+	self.wheelDelta.x = self.newWheelDelta.x
+	self.wheelDelta.y = self.newWheelDelta.y
+	self.newWheelDelta.x = 0
+	self.newWheelDelta.y = 0
 
-	-- TODO dz in windows should be scaled down ... alot
+	self.lastPos.x = self.pos.x
+	self.lastPos.y = self.pos.y
+	self.pos.x = tonumber(self.newPixelPos.x) / tonumber(app.width)
+	self.pos.y = tonumber(self.newPixelPos.y) / tonumber(app.height)
 	self.deltaPos.x = self.pos.x - self.lastPos.x
 	self.deltaPos.y = self.pos.y - self.lastPos.y
 
-	-- rest of the story
 
 	self.lastLeftDown = self.leftDown
 	self.lastRightDown = self.rightDown
-	self.leftDown = bit.band(sdlButtons, sdl.SDL_BUTTON_LMASK) ~= 0
-	self.rightDown = bit.band(sdlButtons, sdl.SDL_BUTTON_RMASK) ~= 0
+	self.leftDown = self.newLeftDown
+	self.rightDown = self.newRightDown
+--print(self.leftDown, self.rightDown)
 
 	-- immediate frame states
 	self.leftClick = false
@@ -112,14 +167,14 @@ function Mouse:update()
 					self.leftClick = true
 				end
 				self.leftDragged = false
-				self.leftDown = false
+				--self.leftDown = false
 			end
 		end
 
 		if self.rightDown then	-- right down
 			if not self.lastRightDown then	-- right press
 				self.rightPress = true
-				self.rightDown = true
+				--self.rightDown = true
 				self.rightDragged = false
 			else
 				if self.deltaPos.x ~= 0 or self.deltaPos.y ~= 0 then
@@ -136,9 +191,85 @@ function Mouse:update()
 					self.rightClick = true
 				end
 				self.rightDragged = false
-				self.rightDown = false
+				--self.rightDown = false
 			end
 		end
+	end
+end
+
+function Mouse:event(e)
+	if e.type == mouseButtonUpType
+	or e.type == mouseButtonDownType
+	then
+		local down = e.type == mouseButtonDownType
+		if e.button.button == sdl.SDL_BUTTON_LEFT then
+			self.newLeftDown = down
+		elseif e.button.button == sdl.SDL_BUTTON_RIGHT then
+			self.newRightDown = down
+		end
+	elseif e.type == mouseMotionEventType then
+		-- if it's a mouse event
+		-- and it came from a touch event
+		-- but we're doing a multi-touch gesture
+		-- then skip processing the mouse event
+		if not (
+			self.doingMultiTouchGesture
+			and e.motion.which == sdl.SDL_TOUCH_MOUSEID
+		) then
+			self.newPixelPos.x = e.motion.x
+			self.newPixelPos.y = e.motion.y
+		end
+	elseif e.type == mouseWheelEventType then
+		self.newWheelDelta.x = self.newWheelDelta.x + e.wheel.x
+		self.newWheelDelta.y = self.newWheelDelta.y + e.wheel.y
+	elseif e.type == fingerDownEventType then
+		local fingerID = tonumber(e.tfinger.fingerID)
+		self.activeFingers[fingerID] = {
+			id = fingerID,
+			pos = vec4f(
+				e.tfinger.x,
+				e.tfinger.y,
+				e.tfinger.dx,
+				e.tfinger.dy
+			),
+		}
+		self.gotFingerEvent = true
+--DEBUG:print('setting finger', fingerID, 'to', self.activeFingers[fingerID].pos)
+	elseif e.type == fingerUpEventType then
+		local fingerID = tonumber(e.tfinger.fingerID)
+--DEBUG:print('clearing finger', fingerID)
+		self.activeFingers[fingerID] = nil
+		self.gotFingerEvent = true
+	elseif e.type == fingerMotionEventType then
+		local fingerID = tonumber(e.tfinger.fingerID)
+		-- looks like sdl3 doesnt have multigesture like sdl2 did
+		-- and sdl3 sends each finger event separately unlike javascript does
+		-- and sdl3 , for query multiple touches with SDL_GetTouchFingers it seems to allocate a structure that needs to be freed ... which I don't want ot do every frame ...
+		local finger = self.activeFingers[fingerID]
+		if not finger then
+			-- motion before down ...
+			self.activeFingers[fingerID] = {
+				id = fingerID,
+				pos = vec4f(
+					e.tfinger.x,
+					e.tfinger.y,
+					e.tfinger.dx,
+					e.tfinger.dy
+				),
+			}
+--DEBUG:print('motion setting finger', fingerID, 'to', self.activeFingers[fingerID].pos)
+		else
+			finger.pos.x, finger.pos.y, finger.pos.z, finger.pos.w
+			= e.tfinger.x, e.tfinger.y, e.tfinger.dx, e.tfinger.dy
+--DEBUG:print('updating finger', fingerID, 'to', finger.pos)
+		end
+		-- what about finger events that dont get a down event?
+		-- should I track time on fingers and clear them periodically?
+
+		-- TODO how often to process events?
+		-- if I do every event, and SDL sends n separate events for n touches, then I'll have n x processing than I need
+		-- so I guess I should process multitouch in :update()
+		self.gotFingerEvent = true
 	end
 end
 
--- orbit.lua
+++ orbit.lua
@@ -12,17 +12,10 @@ local Mouse = require 'glapp.mouse'
 -- a case for a preprocessing ext.load shim layer ...
 local keyUpEventType
 local keyDownEventType
-local mouseMotionEventType
-local mouseWheelEventType
 local handleKeyUpDown
-local fingerUpEventType
-local fingerDownEventType
-local fingerMotionEventType
 if SDLApp.sdlMajorVersion == 2 then
 	keyUpEventType = sdl.SDL_KEYUP
 	keyDownEventType = sdl.SDL_KEYDOWN
-	mouseMotionEventType = sdl.SDL_MOUSEMOTION
-	mouseWheelEventType = sdl.SDL_MOUSEWHEEL
 	function handleKeyUpDown(self, eventPtr)
 		local down = eventPtr[0].type == keyDownEventType
 		if eventPtr[0].key.keysym.sym == sdl.SDLK_LSHIFT then
@@ -42,11 +35,6 @@ if SDLApp.sdlMajorVersion == 2 then
 elseif SDLApp.sdlMajorVersion == 3 then
 	keyUpEventType = sdl.SDL_EVENT_KEY_UP
 	keyDownEventType = sdl.SDL_EVENT_KEY_DOWN
-	mouseMotionEventType = sdl.SDL_EVENT_MOUSE_MOTION
-	mouseWheelEventType = sdl.SDL_EVENT_MOUSE_WHEEL
-	fingerUpEventType = sdl.SDL_EVENT_FINGER_UP
-	fingerDownEventType = sdl.SDL_EVENT_FINGER_DOWN
-	fingerMotionEventType = sdl.SDL_EVENT_FINGER_MOTION
 	function handleKeyUpDown(self, eventPtr)
 		local down = eventPtr[0].type == keyDownEventType
 		if eventPtr[0].key.key == sdl.SDLK_LSHIFT then
@@ -86,7 +74,7 @@ return function(cl)
 	function cl:init(...)
 		cl.super.init(self, ...)
 
-		self.mouse = self.mouse or Mouse()
+		self.mouse = self.mouse or Mouse{app=self}
 
 		self.leftShiftDown = false
 		self.rightShiftDown = false
@@ -94,62 +82,44 @@ return function(cl)
 		self.rightGuiDown = false
 		self.leftAltDown = false
 		self.rightAltDown = false
-
-		self.activeFingers = {}
-		self.activeFingersInOrder = table()
 	end
 
+	-- hmm as a behavior this is tough
+	-- because mouse:update shuold always go last ...
+	-- even after the child class :update ...
 	function cl:update(...)
-		-- process multi-finger events:
-		self.doingMultiTouchGesture = nil
-		if self.gotFingerEvent then
-			local numTouches = 0
-			local prevNumTouches = #self.activeFingersInOrder
-			-- is there a promise that activeFingers IDs are sequential?
-			for fingerID, finger in pairs(self.activeFingers) do
-				numTouches=numTouches+1
-				self.activeFingersInOrder[numTouches] = finger
-			end
-			for j=numTouches+1,prevNumTouches do
-				self.activeFingersInOrder[j] = nil
-			end
---DEBUG:print('we currently have', numTouches, 'touches, previously', prevNumTouches)
+		local mouse = self.mouse
+		mouse:update()
+		cl.super.update(self, ...)
 
-			local f1, f2, pos1, pos2
-			if numTouches >= 2 then
-				f1, f2 = table.unpack(self.activeFingersInOrder)
-				pos1, pos2 = f1.pos, f2.pos
-				self.multiTouchDistSq = (pos2.x - pos1.x)^2 + (pos2.y - pos1.y)^2
-			else
-				self.multiTouchDistSq = nil
-			end
-
-			if self.multiTouchDistSq
-			and self.lastMultiTouchDistSq
-			and self.multiTouchDistSq ~= self.lastMultiTouchDistSq
-			then
-				local toDist = math.sqrt(self.multiTouchDistSq)
-				local fromDist = math.sqrt(self.lastMultiTouchDistSq)
-				local zoomChange = toDist - fromDist
---DEBUG:print('got zoom change', zoomChange, 'from-dist', fromDist, 'to-pts', pos1, pos2, 'to-dist', toDist)
-				-- process a zoom ...
-				-- TODO instead of calling orbit:mouseDownEvent, since it only handles rotate vs pan vs zoom, how about name its functions that?
-				-- zoom
-				self:mouseDownEvent(0, 100 * zoomChange, true, nil, nil, pos1.x, pos1.y)
-				-- tell our mouse event handler to ignore events coming from touches
-				self.doingMultiTouchGesture = true
-			end
-			self.lastMultiTouchDistSq = self.multiTouchDistSq
-			self.gotFingerEvent = nil
+		if mouse.fingerPinchDelta ~= 0 then
+			local pos1 = mouse.activeFingersInOrder[1].pos
+			self:mouseDownEvent(0, mouse.fingerPinchDelta, true, nil, nil, pos1.x, pos1.y)
 		end
 
-		if self.mouse then	-- event() is called before init()
-			self.mouse:update()
+		local shiftDown = self.leftShiftDown or self.rightShiftDown
+		local guiDown = self.leftGuiDown or self.rightGuiDown
+		local altDown = self.leftAltDown or self.rightAltDown
+
+		if mouse.wheelDelta.x ~= 0
+		or mouse.wheelDelta.y ~= 0
+		then
+			self:mouseDownEvent(mouse.wheelDelta.x, mouse.wheelDelta.y, shiftDown, guiDown, altDown, mouse.pos.x, mouse.pos.y)
 		end
-		return cl.super.update(self, ...)
+
+		if (
+			mouse.deltaPos.x ~= 0
+			or mouse.deltaPos.y ~= 0
+		)
+		and mouse.leftDown
+		--and not guiDown
+		then
+			self:mouseDownEvent(mouse.deltaPos.x, mouse.deltaPos.y, shiftDown, guiDown, altDown, mouse.pos.x, mouse.pos.y)
+		end
 	end
 
 	function cl:event(e)
+		local mouse = self.mouse
 		cl.super.event(self, e)
 
 		local canHandleMouse = true
@@ -160,89 +130,14 @@ return function(cl)
 			--canHandleKeyboard = not ig.igGetIO()[0].WantCaptureKeyboard
 		end
 
+		if canHandleMouse then
+			mouse:event(e)
+		end
+
 		local shiftDown = self.leftShiftDown or self.rightShiftDown
 		local guiDown = self.leftGuiDown or self.rightGuiDown
 		local altDown = self.leftAltDown or self.rightAltDown
-		if e.type == mouseMotionEventType
-		or e.type == mouseWheelEventType
-		then
-			if canHandleMouse
-			-- if it's a mouse event
-			-- and it came from a touch event
-			-- but we're doing a multi-touch gesture
-			-- then skip processing the mouse event
-			and not (
-				self.doingMultiTouchGesture
-				and e.motion.which == sdl.SDL_TOUCH_MOUSEID
-			)
-			then
-				local dx, dy, x, y
-				if e.type == mouseMotionEventType then
-					x = e.motion.x
-					y = e.motion.y
-					dx = e.motion.xrel
-					dy = e.motion.yrel
-				else
-					x = e.wheel.x
-					y = e.wheel.y
-					dx = 10 * e.wheel.x
-					dy = 10 * e.wheel.y
-				end
-				if (self.mouse and self.mouse.leftDown and not guiDown)
-				or e.type == mouseWheelEventType
-				then
-					self:mouseDownEvent(dx, dy, shiftDown, guiDown, altDown, x, y)
-				end
-			end
-		elseif e.type == fingerDownEventType then
-			local fingerID = tonumber(e.tfinger.fingerID)
-			self.activeFingers[fingerID] = {
-				id = fingerID,
-				pos = vec4f(
-					e.tfinger.x,
-					e.tfinger.y,
-					e.tfinger.dx,
-					e.tfinger.dy
-				),
-			}
-			self.gotFingerEvent = true
---DEBUG:print('setting finger', fingerID, 'to', self.activeFingers[fingerID].pos)
-		elseif e.type == fingerUpEventType then
-			local fingerID = tonumber(e.tfinger.fingerID)
---DEBUG:print('clearing finger', fingerID)
-			self.activeFingers[fingerID] = nil
-			self.gotFingerEvent = true
-		elseif e.type == fingerMotionEventType then
-			local fingerID = tonumber(e.tfinger.fingerID)
-			-- looks like sdl3 doesnt have multigesture like sdl2 did
-			-- and sdl3 sends each finger event separately unlike javascript does
-			-- and sdl3 , for query multiple touches with SDL_GetTouchFingers it seems to allocate a structure that needs to be freed ... which I don't want ot do every frame ...
-			local finger = self.activeFingers[fingerID]
-			if not finger then
-				-- motion before down ...
-				self.activeFingers[fingerID] = {
-					id = fingerID,
-					pos = vec4f(
-						e.tfinger.x,
-						e.tfinger.y,
-						e.tfinger.dx,
-						e.tfinger.dy
-					),
-				}
---DEBUG:print('motion setting finger', fingerID, 'to', self.activeFingers[fingerID].pos)
-			else
-				finger.pos.x, finger.pos.y, finger.pos.z, finger.pos.w
-				= e.tfinger.x, e.tfinger.y, e.tfinger.dx, e.tfinger.dy
---DEBUG:print('updating finger', fingerID, 'to', finger.pos)
-			end
-			-- what about finger events that dont get a down event?
-			-- should I track time on fingers and clear them periodically?
-
-			-- TODO how often to process events?
-			-- if I do every event, and SDL sends n separate events for n touches, then I'll have n x processing than I need
-			-- so I guess I should process multitouch in :update()
-			self.gotFingerEvent = true
-		elseif e.type == keyUpEventType
+		if e.type == keyUpEventType
 		or e.type == keyDownEventType
 		then
 			handleKeyUpDown(self, e)
@@ -250,7 +145,6 @@ return function(cl)
 	end
 
 	function cl:mouseDownEvent(dx, dy, shiftDown, guiDown, altDown, x, y)
-		if dx == 0 and dy == 0 then return end
 		if shiftDown then
 			if self.view.ortho then
 				-- ortho = shrink / grow view size
@@ -266,23 +160,23 @@ return function(cl)
 				local aspectRatio = self.width / self.height
 				local rx = aspectRatio * (self.mouse.pos.x - .5) * 2
 				local ry = (self.mouse.pos.y - .5) * 2
-				local rx2 = aspectRatio * (x / self.width - .5) * 2
-				local ry2 = ((1 - y / self.height) - .5) * 2
+				local rx2 = aspectRatio * (x - .5) * 2
+				local ry2 = ((1 - y) - .5) * 2
 				local angle = math.asin((rx2 * ry - ry2 * rx) / math.sqrt((rx^2 + ry^2) * (rx2^2 + ry2^2)))
 				local rotation = quatd():fromAngleAxis(0, 0, 1, math.deg(angle))
 				self.view.angle = (self.view.angle * rotation):normalize()
 			else
 				-- frustum = move orbit center
 				local dist = (self.view.pos - self.view.orbit):length()
-				self.view.orbit = self.view.orbit + self.view.angle:rotate(vec3d(-dx,dy,0) * (dist / self.height))
+				self.view.orbit = self.view.orbit + self.view.angle:rotate(vec3d(-dx, dy, 0) * dist)
 				self.view.pos = self.view.angle:zAxis() * dist + self.view.orbit
 			end
 		else
 			if self.view.ortho then
 				-- ortho = drag
 				local aspectRatio = self.width / self.height
-				local fdx = -2 * dx / self.width * self.view.orthoSize * aspectRatio
-				local fdy = 2 * dy / self.height * self.view.orthoSize
+				local fdx = -2 * dx * self.view.orthoSize * aspectRatio
+				local fdy = 2 * dy * self.view.orthoSize
 				self.view.pos = self.view.pos + self.view.angle:rotate(vec3d(fdx, fdy, 0))
 			else
 				-- frustum = rotate around orbit
